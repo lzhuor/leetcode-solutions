4c63014693ee63e32b3cc1fd2b5be3d8af58736b
Zhuoran John <zhuoran666@gmail.com>
2018-05-10 01:39:03 +0800

    Update #4 Median of Two Sorted Arrays.js

--- leetcode-js-solutions/#4 Median of Two Sorted Arrays.js
+++ leetcode-js-solutions/#4 Median of Two Sorted Arrays.js
@@ -4,25 +4,41 @@
  * @return {number}
  */
 var findMedianSortedArrays = function(nums1, nums2) {
-    var mergedArray = [];
-    if(nums1[nums1.length-1] < nums2[0]) {
-        mergedArray.concat(nums1, nums2);
-        return getMedian(mergedArray);
-    } else if (nums1[0] > nums2[nums2.length-1]) {
-        mergedArray.concat(nums2, nums1);
-        return getMedian(mergedArray);
-    } else {
-        
+  let nextIndex = 0;
+  nums1.forEach(num1 => {
+    for (let i = nextIndex; i <= nums2.length; i++) {
+      if (i === nums2.length) {
+        nums2.splice(i, 0, num1);
+        break;
+      }
+
+      if (num1 < nums2[i]) {
+        nums2.splice(i, 0, num1);
+        nextIndex = i + 1;
+        break;
+      }
     }
+  })
+
+  return getMedian(nums2);
 };
 
-var getMedian = (mergedArray) => {
-    if(mergedArray.length % 2 === 0 && mergedArray.length !== 0) {
-        let lowerPosition = mergedArray.length / 2;
-        return ( mergedArray[lowerPosition] + mergedArray[lowerPosition + 1] ) / 2;
-    } else if(mergedArray.length % 2 === 1) {
-        return mergedArray[ mergedArray.length / 2 ];
-    } else {
-        return 0;
-    }
-};
+var getMedian = sortedNums => {
+  let median;
+
+  const halfIndex = Math.floor(sortedNums.length / 2);
+
+  if (sortedNums.length % 2 !== 0) {
+    median = sortedNums[halfIndex];
+  } else {
+    median = (sortedNums[halfIndex] + sortedNums[halfIndex - 1]) / 2
+  }
+
+  return median;
+}
+
+// Test Case
+const result = findMedianSortedArrays([1, 2,], [3, 4]);
+console.log('result:', result); // 2.5 
+
+
