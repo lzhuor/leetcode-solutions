16f87477e5759d6577d3d5247ca38ac719f203b3
Zhuoran John <mail@zhuoran.li>
2018-05-25 02:27:00 +0800

    Create Problem6ZigZagConversion.java

---
+++ leetcode-java-solutions/src/test/java/Problem6ZigZagConversion.java
@@ -0,0 +1,66 @@
+import org.eclipse.collections.api.map.MutableMap;
+import org.eclipse.collections.api.tuple.Pair;
+import org.eclipse.collections.impl.factory.Maps;
+import org.eclipse.collections.impl.tuple.Tuples;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Problem6ZigZagConversion {
+    @Test
+    public void ZigZagConversion() {
+        MutableMap<Pair<String, Integer>, String> testCases = Maps.mutable.with(
+                // case 1
+                Tuples.pair("PAYPALISHIRING", 3), "PAHNAPLSIIGYIR",
+                // case 2
+                Tuples.pair("PAYPALISHIRING", 4), "PINALSIGYAHRPI"
+        );
+
+        testCases.forEachKeyValue((pair, result) -> assertThat(convert(pair.getOne(), pair.getTwo())).isEqualTo(result));
+    }
+
+    private String convert(String s, Integer rows) {
+        if (rows == 1) return s;
+
+        String[] arr = s.split("");
+        List<List<String>> list = new ArrayList<>();
+
+        int counter = 0;
+        while (counter < rows) {
+            list.add(new ArrayList<>());
+            counter++;
+        }
+
+        int row = 0;
+        boolean isForward = true;
+
+        for (String c : arr) {
+            if (isForward) {
+                list.get(row).add(c);
+                row++;
+            } else {
+                list.get(row).add(c);
+                row--;
+            }
+
+            if (row == rows) {
+                isForward = false;
+                row-=2;
+            }
+            if (row == 0) {
+                isForward = true;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder(s.length());
+
+        for (List<String> r : list) {
+            r.forEach(sb::append);
+        }
+
+        return sb.toString();
+    }
+}
