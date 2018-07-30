d863bf97e2410d2f29b8b8798d7fdba8953355d3
John <john@stashaway.com>
2018-07-30 16:31:25 +0800

    feat(core): Added more testes cases of Problem 9 & Fixed bugs

--- leetcode-java-solutions/src/test/java/Problem9PalindromeNumber.java
+++ leetcode-java-solutions/src/test/java/Problem9PalindromeNumber.java
@@ -9,7 +9,10 @@ public class Problem9PalindromeNumber {
         assertTrue(isPalindrome(0));
         assertTrue(isPalindrome(121));
         assertTrue(isPalindrome(1212121));
+        assertFalse(isPalindrome(10));
+        assertFalse(isPalindrome(1000));
         assertFalse(isPalindrome(-121));
+        assertFalse(isPalindrome(1000030001));
     }
 
     private boolean isPalindrome(int x) {
@@ -17,11 +20,16 @@ public class Problem9PalindromeNumber {
 
         String numString = Integer.toString(x);
         char[] numStringArr = numString.toCharArray();
+        boolean isLengthEven = numStringArr.length % 2 == 0;
 
-        for (int i = 0, j = numStringArr.length - 1; i + 1 <= j - 1; i++, j--) {
+        for (int i = 0, j = numStringArr.length - 1; isLengthEven ? i + 1 <= j : i <= j; i++, j--) {
             if (numStringArr[i] != numStringArr[j]) {
                 return false;
             }
+        }
+
+        if (numStringArr.length == 2) {
+            return numStringArr[0] == numStringArr[1];
         }
 
         return true;
